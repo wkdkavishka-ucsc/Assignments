ca931b73aa8b352861ff483638b0f652087fabd3
wkdkavishka-ucsc <84147387+wkdkavishka-ucsc@users.noreply.github.com>
2021-08-12 20:46:43 +0530

    update febonachi function

--- recursive_activity/q6.scala
+++ recursive_activity/q6.scala
@@ -5,11 +5,27 @@ import util.control.Breaks._
 import scala.collection.mutable._
 
 object q6{
-    def feb(num:Int = 10, pre:Int = 0): Unit = {
-        if ( num ==  )
+    def feb(num:Int = 10, num1:Int = 0, num2:Int = 1): Unit = {// febonachi ( print )
+        var sum = num1+num2; // 
+        if(num > sum) {// sum is less than the number
+            print(s" -> $sum");
+            feb(num,num2,(num1+num2));
+        }
+        else if(num==(num1+num2)){// number is the sum
+            print(f" -> $num");  return;
+        }
+        else {// sum is beyond 
+            return;
+        }
     }
 
     def main(args:Array[String]): Unit = {// main
-
+        while(true){// infinite loop
+            print("number -> ");
+            var number = readInt();
+            print(" 0 ");
+            feb(number);
+            print("\n");
+        }
     }
 }
